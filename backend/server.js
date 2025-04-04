fc0beb92dcfee36c759d730d781f3ea0b2712513
Ernest Rimkevicius <65626331+Urnesto@users.noreply.github.com>
2025-04-04 15:20:12 +0300

    commited  api log post req and api logs get req

--- backend/server.js
+++ backend/server.js
@@ -9,17 +9,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose
-  .connect(process.env.MONGO_URI, {})
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect(process.env.MONGO_URI, {})
+//   .then(() => console.log("Connected to MongoDB"))
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
-const LogSchema = new mongoose.Schema({
-  cityName: String,
-  timestamp: { type: Date, default: Date.now },
-});
+// const LogSchema = new mongoose.Schema({
+//   cityName: String,
+//   timestamp: { type: Date, default: Date.now },
+// });
 
-const Log = mongoose.model("Log", LogSchema);
+// const Log = mongoose.model("Log", LogSchema);
 
 app.get("/api/places", async (req, res) => {
   try {
@@ -47,36 +47,36 @@ app.post("/api/forecast", async (req, res) => {
   }
 });
 
-app.post("/api/log", async (req, res) => {
-  const cityName = req.body.cityName;
+// app.post("/api/log", async (req, res) => {
+//   const cityName = req.body.cityName;
 
-  if (!cityName) {
-    return res.status(400).json({ error: "cityName is required" });
-  }
+//   if (!cityName) {
+//     return res.status(400).json({ error: "cityName is required" });
+//   }
 
-  try {
-    // Create new log entry in MongoDB
-    const log = new Log({ cityName });
-    await log.save();
+//   try {
+//     // Create new log entry in MongoDB
+//     const log = new Log({ cityName });
+//     await log.save();
 
-    const timestamp = new Date().toISOString();
-    console.log(`[${timestamp}] City selected: ${cityName}`);
+//     const timestamp = new Date().toISOString();
+//     console.log(`[${timestamp}] City selected: ${cityName}`);
 
-    res.json({ message: "Action logged successfully" });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to save log" });
-  }
-});
+//     res.json({ message: "Action logged successfully" });
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to save log" });
+//   }
+// });
 
-// endpoint to get all logs from DB
-app.get("/api/logs", async (req, res) => {
-  try {
-    const logs = await Log.find().sort({ timestamp: -1 }); // Get all logs, sorted by timestamp (newest first)
-    res.json(logs);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch logs" });
-  }
-});
+// // endpoint to get all logs from DB
+// app.get("/api/logs", async (req, res) => {
+//   try {
+//     const logs = await Log.find().sort({ timestamp: -1 }); // Get all logs, sorted by timestamp (newest first)
+//     res.json(logs);
+//   } catch (error) {
+//     res.status(500).json({ error: "Failed to fetch logs" });
+//   }
+// });
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Weather api healthy", process.env.PORT || 8080);
