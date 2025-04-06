738305d43d1296aecdf31ba7036919098d90be39
Ernest Rimkevicius <65626331+Urnesto@users.noreply.github.com>
2025-04-06 19:25:17 +0300

    fixed backend url

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://3.78.187.71:8080";
-
-console.log(BASE_URL);
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const api = axios.create({
   baseURL: `${BASE_URL}/api`,
